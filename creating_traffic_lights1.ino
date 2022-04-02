8eb55bc71f716ba604daefc6165672ca9c5f5b7a
Syed Shoaib UR Rehman <86778601+sshoaib456@users.noreply.github.com>
2022-04-02 13:42:46 +0500

    Add files via upload

---
+++ creating_traffic_lights1.ino
@@ -0,0 +1,66 @@
+// C++ code
+//
+void setup()
+{
+  pinMode(0 , OUTPUT);
+  pinMode(2 , OUTPUT);
+  pinMode(3 , OUTPUT);
+  pinMode(4 , OUTPUT);
+  pinMode(7 , OUTPUT);
+  
+}
+
+void loop()
+{
+  digitalWrite(0, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(0, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+  
+  digitalWrite(2, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(2, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+ 
+  
+  digitalWrite(3,HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(3,LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+ 
+  
+  digitalWrite(4,HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(4,LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+  
+  
+  digitalWrite(7,HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(7,LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+  
+  
+  digitalWrite(4,HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(4,LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+  
+   digitalWrite(3,HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(3,LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+  
+    digitalWrite(2, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(2, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+  
+  
+ 
+  
+  
+  
+  
+  
+}
